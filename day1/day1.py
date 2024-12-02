f259163efb63cf783aac76748360f6ffa458612c
mariebmo <marie.stigen@hotmail.com>
2024-12-02 10:48:03 +0100

    Solved Day1-B

--- day1/day1.py
+++ day1/day1.py
@@ -10,7 +10,6 @@ def A():
                 if len(split) != 2:
                     continue  # Skip lines that do not have exactly two elements
                 left, right = split
-                print(left, right)
                 leftList.append(int(left))
                 rightList.append(int(right))
 
@@ -28,4 +27,42 @@ def A():
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def B():
+    try:
+        with open("./files/day1.txt", "r") as f:
+            amountOfTimesLeft = {}
+            amountOfTimesRight = {}
+
+            similarityScore = 0
+
+            for line in f:
+                split = line.split()
+                if len(split) != 2:
+                    continue  # Skip lines that do not have exactly two elements
+                left, right = split
+                if left in amountOfTimesLeft:
+                    amountOfTimesLeft[left] += 1
+                else:
+                    amountOfTimesLeft[left] = 1
+
+                if right in amountOfTimesRight:
+                    amountOfTimesRight[right] += 1
+                else:
+                    amountOfTimesRight[right] = 1
+
+
+        ## if 1 is 3  times in left list, and 2 times in right list, the similarity score is (1 * 2) * 3
+        for key in amountOfTimesLeft:
+            if key in amountOfTimesRight:
+                similarityScore += int(key) * amountOfTimesLeft[key] * amountOfTimesRight[key]
+        
+        print(similarityScore)
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+    except ValueError:
+        print("File contains non-integer values.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 A()
+B()
