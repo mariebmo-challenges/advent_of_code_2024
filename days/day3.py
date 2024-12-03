3cc51823e22478da5667adabb3cd862ce7445957
mariestigen <marie.stigen@hotmail.com>
2024-12-03 17:03:53 +0100

    Finished Day 3-2

--- days/day3.py
+++ days/day3.py
@@ -12,7 +12,6 @@ def A():
                 firstNum = int(split[0][4:])
                 secondNum = int(split[1][:-1])
                 total += firstNum * secondNum
-            
 
             print(total)
 
@@ -24,3 +23,34 @@ def A():
         print(f"An error occurred: {e}")
 
 A()
+
+def B():
+    try:
+        with open("./files/day3.txt", "r") as f:
+            results = re.findall(r"(mul\(\d+\,\d+\))|(do\(\))|(don't\(\))", f.read())
+            shouldDo = True
+            total = 0
+
+            # 2463154 too low
+
+            for result in results:
+                if(result[0].startswith("mul") and shouldDo):
+                    split = result[0].split(',')
+                    firstNum = int(split[0][4:])
+                    secondNum = int(split[1][:-1])
+                    total += firstNum * secondNum
+                elif(result[1].startswith("do")):
+                    shouldDo = True
+                elif(result[2].startswith("don't")):
+                    shouldDo = False
+
+            print(total)
+
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+    except ValueError:
+        print("File contains non-integer values.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+B()
