dd5180df01624681b8ec772bd36f218dc8104adb
mariestigen <marie.stigen@hotmail.com>
2024-12-04 21:41:23 +0100

    Did 4-2

--- days/day4.py
+++ days/day4.py
@@ -76,4 +76,61 @@ def findXmas(data):
   
     return total
 
-A()
+# A()
+
+def B():
+    try:
+        with open("./files/day4.txt", "r") as f:
+            # find each apperance of regex mul\(\d+\,\d+\)
+            total = 0
+
+            data = f.read()
+
+            total = findMasInX(data)
+
+            print(total)
+
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+    except ValueError:
+        print("File contains non-integer values.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def findMasInX(data):
+
+    total = 0
+
+    lines = data.split("\n")
+
+    for line in range(len(lines)):
+        for row in range(len(lines[line])):
+
+            currentChar = lines[line][row]
+            if(currentChar == "A"):
+
+                if (line + 1 < len(lines) and row + 1 < len(lines[line]) and line - 1 >= 0 and row - 1 >= 0):
+                    upperLeft = lines[line - 1][row - 1]
+                    upperRight = lines[line - 1][row + 1]
+                    lowerLeft = lines[line + 1][row - 1]
+                    lowerRight = lines[line + 1][row + 1]
+                    
+                    if(upperLeft == "M" and upperRight == "M" and lowerLeft == "S" and lowerRight == "S"):
+                        print("Found MAS in X starting on line " + str(line) + " row " + str(row))
+                        total += 1
+                    
+                    if(upperLeft == "S" and upperRight == "S" and lowerLeft == "M" and lowerRight == "M"):
+                        print("Found MAS in X starting on line " + str(line) + " row " + str(row))
+                        total += 1
+
+                    if(upperLeft == "M" and upperRight == "S" and lowerRight == "S" and lowerLeft == "M"):
+                        print("Found MAS in X starting on line " + str(line) + " row " + str(row))
+                        total += 1
+
+                    if(upperLeft == "S" and upperRight == "M" and lowerRight == "M" and lowerLeft == "S"):
+                        print("Found MAS in X starting on line " + str(line) + " row " + str(row))
+                        total += 1
+                    
+    return total
+                
+B()
